eab131ada4e24e48735488e84b2a7e831f08d19f
sjwon0368 <82024430+sjwon0368@users.noreply.github.com>
2022-11-18 11:07:59 +0900

    Initial Commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import LoaderGIF from '@public/Loader_Blue_a098n32.gif'
+import LoaderGIF from '../public/Loader_Blue_a098n32.gif'
 
 export default function Home() {
   return (
