f58dba092dd20cbc393747c8fdb95c489383ca54
sjwon0368 <82024430+sjwon0368@users.noreply.github.com>
2022-11-18 09:42:47 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,8 @@ export default function Home() {
         <Header title="한컴타자 다이렉트링크" />
         <p className="description">
           잠시만 기다려주세요! 한컴타자 서버에 연결하는 중입니다...
-        </p>
+        </p><br/>
+        <img src='./public/Loader_Blue_a098n32' />
       </main>
 
       <Footer />
